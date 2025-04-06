0f75191536003022b70836a3d98ea7b657d75f3e
jimwei6 <jimwei6@gmail.com>
2025-04-06 05:11:45 -0700

    Remove softmax dqn

--- DQN.py
+++ DQN.py
@@ -74,9 +74,7 @@ class DQN(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Flatten(),
-            nn.Linear(32 * 11 * 11, output_dim), 
-            nn.Softmax(dim=-1)
-        )
+            nn.Linear(32 * 11 * 11, output_dim)        )
     
     def recall(self, batch_size):
         batch = self.memory.sample(batch_size)
